51f8e93ef2e97e3cd022e01f61f4443321f1e9e1
Szo Viktor <szoviktor40@gmail.com>
2024-02-20 13:19:01 +0100

    Tovabbi feladatok kerultek megoldasra

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -40,18 +40,38 @@ namespace ConsoleApp1
             Console.WriteLine($"A legtöbb villám a(z) {maxNap + 1}. napon volt, " +
                               $"a(z) {Array.IndexOf(osszeg, maxVillam)}. órában: {maxVillam} villám.");
 
-            var result = villamlasok
+            var result4 = villamlasok
                 .Select((v, i) =>
                 {
                     var index = v.Orak.FindIndex(x => x > 0);
-                    return $"{i + 1};{(index != -1 ? index.ToString() : "null")}";
+                    return $"{i + 1};{(index != -1 ? index+1 : "null")}";
                 });
 
-            File.WriteAllLines("villam_napok_ora.txt", result);
+            File.WriteAllLines("../../../src/villam_napok_ora.txt", result4);
 
-            File.WriteAllLines("../../../src/villam_napok_ora.txt", result);
+            int result5 = villamlasok.SelectMany(v => v.Orak).Count(e => e > 0);
 
-            var hanyoravolt = villamlasok.Count(x => x.)
+            Console.WriteLine($"Összesen {result5} olyan óra volt, amikor villámlott.");
+
+            int result6 = villamlasok.FindIndex(v => v.Orak.Sum() < 200);
+
+            if (result6 != -1)
+                Console.WriteLine($"Az első nap, amikor 200-nál kevesebb villámlás volt: {result6 + 1}. nap.");
+            else
+                Console.WriteLine("Nem található olyan nap, ahol 200-nál kevesebb villámlás volt.");
+
+            int result7 = villamlasok.FindIndex(e => e.Orak.Sum() == 0);
+
+            if (result7 == 0)
+            {
+                Console.WriteLine("HIBA! Minden nap villámlott.");
+            }
+            else Console.WriteLine($"A {result7 + 1}. napon nem volt villámlás");
+
+            int result8 = villamlasok.Count(v => v.Orak[0] > 0 && v.Orak[1] > 0);
+
+            Console.WriteLine($"{result8 * 2} óra volt");
+
         }
 
     }
