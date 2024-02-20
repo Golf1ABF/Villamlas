eacd84ae65e469fd77507d352f6e57c7ee1117b6
c20szóv <c20szv@203-15>
2024-02-20 12:39:16 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using ConsoleApp1;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Villamlas> villamlasok = new List<Villamlas>();
+
+            using (StreamReader sr = new StreamReader(@"../../../src/villam.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    villamlasok.Add(new Villamlas(sr.ReadLine()));
+                }
+            }
+
+            foreach (var item in villamlasok)
+            {
+                Console.WriteLine(item);
+            }
+
+            int[] osszeg = new int[villamlasok.Count()];
+            int maxVillam = 0;
+            int maxNap = 0;
+
+            for (int i = 0; i < villamlasok.Count; i++)
+            {
+                int villamokSzama = villamlasok[i].Orak.Max();
+                osszeg[i] = villamokSzama;
+
+                if (villamokSzama > maxVillam)
+                {
+                    maxVillam = villamokSzama;
+                    maxNap = i;
+                }
+            }
+
+            Console.WriteLine($"A legtöbb villám a(z) {maxNap + 1}. napon volt, " +
+                              $"a(z) {Array.IndexOf(osszeg, maxVillam)}. órában: {maxVillam} villám.");
+
+            var result = villamlasok
+                .Select((v, i) =>
+                {
+                    var index = v.Orak.FindIndex(x => x > 0);
+                    return $"{i + 1};{(index != -1 ? index.ToString() : "null")}";
+                });
+
+            File.WriteAllLines("villam_napok_ora.txt", result);
+
+            File.WriteAllLines("../../../src/villam_napok_ora.txt", result);
+
+            var hanyoravolt = villamlasok.Count(x => x.)
+        }
+
+    }
+}
---
+++ ConsoleApp1/Villamlas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Villamlas
+    {
+        public int Augusztusnapjai { get; set; }
+        public List<int> Orak { get; set; }
+
+        public Villamlas(string sor)
+        {
+            string[] s = sor.Split(";");
+            this.Augusztusnapjai = int.Parse(s[0]);
+            this.Orak = new List<int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                Orak.Add(int.Parse(s[i]));
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(";", Orak);
+        }
+    }
+}
